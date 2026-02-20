5fa18ff5f28f5bd80c5d7a240e4e938fe3ca8011
SwiftlyNoah <devnoahapps@gmail.com>
2026-02-19 22:40:56 -0500

    Initial Commit

---
+++ TheCrimson/TheCrimsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCrimsonApp.swift
+//  TheCrimson
+//
+//  Created by Noah Brauner on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TheCrimsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
